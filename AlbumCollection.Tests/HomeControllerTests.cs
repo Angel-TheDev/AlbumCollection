68ac331b449c26f950c3072e124cf73ecbe66cfb
ladejesus <luisito.dejesus@gmail.com>
2019-07-09 10:23:50 -0400

    added MVC scratch model

---
+++ AlbumCollection.Tests/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlbumCollection.Tests
+{
+    class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_Hello_World()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.Equal("Hello world!", result);
+        }
+
+    }
+}
