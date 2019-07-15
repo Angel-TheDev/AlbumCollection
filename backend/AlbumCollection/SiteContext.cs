04034449d2b968c71de6cdff4337ed672d721101
CLux <cplux7@gmail.com>
2019-07-15 09:55:50 -0400

    added sitecontext and models, populated the album model with info for creating objects.

---
+++ backend/AlbumCollection/SiteContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumCollection
+{
+    public class SiteContext : DbContext
+    {
+        public DbSet<Artist>Artists { get; set; }
+        public DbSet<Album>Albums { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=DripCoffeeAlbumCollection;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                          .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>().HasData(
+
+
+    }
+}
