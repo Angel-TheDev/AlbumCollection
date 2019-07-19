cb5106a84a3a23d7f98a3e243d3b28718c75105e
Blair <bgriffit@oberlin.edu>
2019-07-19 10:25:19 -0400

    starting work on components

--- parcel-frontend/js/components/artistImages.js
+++ parcel-frontend/js/components/artistImages.js
@@ -1,10 +1,10 @@
 import images from "~/images/*.jpg"
 //<img src= "${artistImages}"
-export default function ArtistImages(artistImages){
+export default function ArtistImages(){
 
         {Object.keys(images).map(key => {
+            console.log(images)
             return `<img key='${key}' src='${images[key]}' alt=""></img>`;
-                    
                     
        
         })
--- parcel-frontend/js/components/artists.js
+++ parcel-frontend/js/components/artists.js
@@ -1,31 +1,23 @@
-import artistImages from "~/images/*.jpg"
-import images from './artistImages'
-//<img src= "${artistImages}"
 export default function Artists(artists){
     return `
     <ul>
-
         ${artists.map(artist => {
             return `
                 <li>
-                    ${images()}
                     <h3>${artist.name}</h3>
                     <input class='artist_id' type='hidden' value='${artist.artistId}'>
                     <input class='artist_name' type='hidden' value='${artist.name}'>
                     <input class='artist_hometown' type='hidden' value='${artist.homeTown}'>
                         <button class='delete-artist'>Delete</button>
                         <button class='edit-artist'>Edit</button>
-                       
-       
-
-    <section class='edit-artist-box'>
-    <input class='artist_id' type='hidden' value='${artist.artistId}'>
-    <input class='edit-artist_name' type='text' value='${artist.name}'>
-        <input class='edit-artist_hometown' type='text' value='${artist.homeTown}'>
-        <button class='edit-artist_submit'>Submit</button>
-    </section>
+                    <section class='edit-artist-box'>
+                    <input class='artist_id' type='hidden' value='${artist.artistId}'>
+                    <input class='edit-artist_name' type='text' value='${artist.name}'>
+                        <input class='edit-artist_hometown' type='text' value='${artist.homeTown}'>
+                        <button class='edit-artist_submit'>Submit</button>
+                    </section>
                 </li>
-            `;
+    `;
         })
         .join("")}
     </ul>
